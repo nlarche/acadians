64c6bcd0a72af365c09e26d4644029d45d9d232b
nlarche <nico060984@gmail.com>
2018-08-16 18:53:18 +0200

    Add date tour

--- init.js
+++ init.js
@@ -1,9 +1,11 @@
 var baseurl = "https://res.cloudinary.com/acadians/image/upload/v1527360791/";
+var dateTourUrl = "https://docs.google.com/spreadsheets/d/1vNvVsd_llw7D962SpSYVnaLo94xnuvBlw_sknxoBN4Q/edit?usp=sharing"
 
 document.addEventListener('DOMContentLoaded', function (){
 
     initGalleryCaroussel();
     initGallery();
+    initDateTour();
 
     $('.popup-image').magnificPopup({
         type:'image'
@@ -16,6 +18,18 @@ document.addEventListener('DOMContentLoaded', function (){
 document.addEventListener('scroll', function (){
     loadImage();
 })
+
+function getJsDateFromExcel(excelDate) {
+
+    // JavaScript dates can be constructed by passing milliseconds
+    // since the Unix epoch (January 1, 1970) example: new Date(12312512312);
+  
+    // 1. Subtract number of days between Jan 1, 1900 and Jan 1, 1970, plus 1 (Google "excel leap year bug")             
+    // 2. Convert to milliseconds.
+  
+      return new Date((excelDate - (25567 + 1))*86400*1000);
+  
+  }
 
 function initGallery(){
     
@@ -54,6 +68,64 @@ function initGalleryCaroussel(){
             navigation : true
         });
     }
+}
+
+function initDateTour(){
+    gsheets.getWorksheet('1vNvVsd_llw7D962SpSYVnaLo94xnuvBlw_sknxoBN4Q', 'date')
+    .then(resolveDateTour, err => console.error(err));
+}
+
+function resolveDateTour(res){
+    if (res.data.length === 0){
+        return;
+    }
+    var parent = document.querySelector("[data-tour-date]");
+    res.data
+        .map(function (d) {
+            d.date = getJsDateFromExcel(d.date);
+            return d;
+        })
+        .forEach(function (d){
+            var article = createElement("article", "post");
+            parent.appendChild(article);
+            article.appendChild(createEventLeft(d));
+            article.appendChild(createEventDetails(d));
+    });
+}
+
+function createEventLeft(data){
+    var divDate = createElement("div", "event_left");
+    var date = createElement("div", "event_date");
+    date.innerText = dateFns.format(data.date, "D");
+    var month = createElement("div", "event_month");
+    month.innerText = dateFns.format(data.date, "MMM");;
+    divDate.appendChild(date);
+    divDate.appendChild(month);
+    return divDate;
+}
+
+function createEventDetails(data){
+    var detail = createElement("div", "event_detail");
+    var title = createElement("h2", "event_title");
+    var titleHref = createElement("a");
+    titleHref.innerText = data.lieu;
+    title.appendChild(titleHref);
+    var time = createElement("div", "event_time");
+    var timeI = createElement("i", "time icon");
+    time.appendChild(timeI);
+    time.appendChild(document.createTextNode(" " + dateFns.format(data.date, "D MMMM YY")));
+    var location = createElement("div", "event_location");
+    var locationI = createElement("i", "map marker icon");
+    location.appendChild(locationI);
+    location.appendChild(document.createTextNode(" " + data.ville));
+    var buy = createElement("a", "event_button ui button colored");
+    buy.href = data.lien;
+    buy.innerText = "+ infos"
+    detail.appendChild(title);
+    detail.appendChild(time);
+    detail.appendChild(location);
+    detail.appendChild(buy);
+    return detail;
 }
 
 function createElement(name, className){
